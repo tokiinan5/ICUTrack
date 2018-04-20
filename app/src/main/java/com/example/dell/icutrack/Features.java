e3da6caa4621e6484027b5016a0bdcf0d4963450
Toki <tokiinan5@gmail.com>
2018-04-20 16:26:41 +0600

    commit

---
+++ app/src/main/java/com/example/dell/icutrack/Features.java
@@ -0,0 +1,111 @@
+package com.example.dell.icutrack;
+
+public class Features {
+    private double pAge,pBillirubin,pPlatelate,pLos,pPaO2,pSodium,pPotassium,pCreatinine,pWbc,pHematocrit,pTemp,pPh;
+
+    public double getpAge() {
+        return pAge;
+    }
+
+    public void setpAge(double pAge) {
+        this.pAge = pAge;
+    }
+
+    public double getpBillirubin() {
+        return pBillirubin;
+    }
+
+    public void setpBillirubin(double pBillirubin) {
+        this.pBillirubin = pBillirubin;
+    }
+
+    public double getpPlatelate() {
+        return pPlatelate;
+    }
+
+    public void setpPlatelate(double pPlatelate) {
+        this.pPlatelate = pPlatelate;
+    }
+
+    public double getpLos() {
+        return pLos;
+    }
+
+    public void setpLos(double pLos) {
+        this.pLos = pLos;
+    }
+
+    public double getpPaO2() {
+        return pPaO2;
+    }
+
+    public void setpPaO2(double pPaO2) {
+        this.pPaO2 = pPaO2;
+    }
+
+    public double getpSodium() {
+        return pSodium;
+    }
+
+    public void setpSodium(double pSodium) {
+        this.pSodium = pSodium;
+    }
+
+    public double getpPotassium() {
+        return pPotassium;
+    }
+
+    public void setpPotassium(double pPotassium) {
+        this.pPotassium = pPotassium;
+    }
+
+    public double getpCreatinine() {
+        return pCreatinine;
+    }
+
+    public void setpCreatinine(double pCreatinine) {
+        this.pCreatinine = pCreatinine;
+    }
+
+    public double getpWbc() {
+        return pWbc;
+    }
+
+    public void setpWbc(double pWbc) {
+        this.pWbc = pWbc;
+    }
+
+    public double getpHematocrit() {
+        return pHematocrit;
+    }
+
+    public void setpHematocrit(double pHematocrit) {
+        this.pHematocrit = pHematocrit;
+    }
+
+    public double getpTemp() {
+        return pTemp;
+    }
+
+    public void setpTemp(double pTemp) {
+        this.pTemp = pTemp;
+    }
+
+    public double getpPh() {
+        return pPh;
+    }
+
+    public void setpPh(double pPh) {
+        this.pPh = pPh;
+    }
+
+    public String getpGender() {
+        return pGender;
+    }
+
+    public void setpGender(String pGender) {
+        this.pGender = pGender;
+    }
+
+    private  String pGender;
+}
